3224f20708e81b52b7b5bb1d8fcc082610ee36e6
n1kol4a <andreevnikola200247@gmail.com>
2025-04-20 05:33:35 +0200

    Add onlyOwner to mint function in BasicNft

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -2,16 +2,17 @@
 
 pragma solidity ^0.8.18;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BasicNft is ERC721 {
+contract BasicNft is ERC721, Ownable {
     uint256 private s_tokenCounter;
     mapping(uint256 => string) private s_tokenIdToUri;
 
-    constructor() ERC721("Dogie", "DOG") {
+    constructor() ERC721("Dog", "Dogie") Ownable(msg.sender) {
         s_tokenCounter = 0;
     }
 
-    function mintNft(string memory tokenUri) public {
+    function mintNft(string memory tokenUri) public onlyOwner {
         s_tokenIdToUri[s_tokenCounter] = tokenUri;
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter++;
